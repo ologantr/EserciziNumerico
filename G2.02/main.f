4b27d2f65554606684e726313fd7d48690b50dd3
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-04-01 17:07:13 +0200

    Add G2.02

---
+++ G2.02/main.f
@@ -0,0 +1,31 @@
+*     Considerare vettori con un numero n di componenti da 2 a 15,
+*     le cui componenti sono punti equidistanti dell'intervallo [-1,1].
+*     Calcolare le tre norme vettoriali al crescere di n e farne il
+*     confronto analizzando il grafico dei risultati.
+
+*     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+      PROGRAM MAIN
+
+*     Numero di componenti
+      PARAMETER (N = 5)
+
+*     Minimo e massimo dell'intervallo dei valori
+      PARAMETER (VAL_MIN = -1.0)
+      PARAMETER (VAL_MAX = +1.0)
+
+      REAL V(N)
+      REAL STEP
+
+      V(1) = VAL_MIN
+      V(N) = VAL_MAX
+
+      STEP = (ABS(VAL_MIN) + ABS(VAL_MAX)) / (N - 1)
+
+      DO I = 2, N - 1
+         V(I) = V(I - 1) + STEP
+      ENDDO
+
+      WRITE(*,*) V
+
+      END
