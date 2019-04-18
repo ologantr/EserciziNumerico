84c19ee9fdd5e6db3ba77568a7d440eb470bee4d
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-04-18 12:18:23 +0200

    Use forked core.matrix that doesn't coerce ratios

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -1,7 +1,13 @@
 (defproject calcolo-numerico "0.1.0-SNAPSHOT"
+
+  :plugins [[reifyhealth/lein-git-down "0.3.2"]]
+
+  :repositories [["github" {:url "git://github.com"}]]
+
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [net.mikera/core.matrix "0.62.0"]]
+                 [turlando/core.matrix "28a1939018df81f93c66fb1fff357e39d0ff8925"]]
+
 
   :repl-options {:init-ns calcolo-numerico.core}
 
