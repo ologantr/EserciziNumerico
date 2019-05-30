2f86fa071ef0965a273a62ccccdc69becac0af90
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-05-30 17:01:24 +0200

    Finish G7.01

--- Python/cn/exercises.py
+++ Python/cn/exercises.py
@@ -1,10 +1,19 @@
 from cn.algebra import evaluate, linspace
 from cn.interpolation import linear_interpolation
+from cn.plot import make_figure
 import cn.functions as fns
 
 def e7_01():
     range_ = [-1, 1]
-    real_points = evaluate(fns.fn_1, linspace(range_, 10))
+    real_points_number = 10
+    interpolated_points_number = 1000
+
+    real_points = evaluate(fns.fn_1, linspace(range_, real_points_number))
+
     poly = linear_interpolation(real_points)
-    interpolated_points = tuple((x, poly(x)) for x in linspace(range_, 100))
-    return interpolated_points
+    interpolated_points = tuple((x, poly(x))
+                                for x in linspace(range_,
+                                                  interpolated_points_number))
+
+    make_figure(real_points).show()
+    make_figure(interpolated_points).show()
