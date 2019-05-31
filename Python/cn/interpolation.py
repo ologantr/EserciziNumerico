c2192cc2184d0538c4578c90c0875c435ce50a26
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-05-31 10:24:42 +0200

    Add working implementation

--- Python/cn/interpolation.py
+++ Python/cn/interpolation.py
@@ -1,6 +1,6 @@
 from cn.arithmetic import product
 
-def linear_interpolation(points):
+def linear_interpolation_(points):
     xs, ys = tuple(zip(*points))
     n = len(points)
 
@@ -46,3 +46,29 @@ def linear_interpolation(points):
         return pi(x) * sum_(x)
 
     return fn
+
+
+def linear_interpolation(points):
+    xs, ys = tuple(zip(*points))
+    n = len(points)
+
+    def basis(j):
+        def numerator(x, i):
+            return x - xs[i]
+
+        def denominator(j, i):
+            return xs[j] - xs[i]
+
+        def fn(x):
+            return product(numerator(x, i) / denominator(j, i)
+                           for i in range(n)
+                           if i != j)
+
+        return fn
+
+    def lagrange(x):
+        bases = tuple(basis(j) for j in range(n))
+        return sum(y * base(x)
+                   for y, base in zip(ys, bases))
+
+    return lagrange
