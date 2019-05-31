574bdcabfb4d3329b697c20a42715c6bf1b0b070
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-05-31 13:00:12 +0200

    make e7_01 more hackable

--- Python/cn/exercises.py
+++ Python/cn/exercises.py
@@ -1,15 +1,17 @@
 from cn.algebra import evaluate, linspace
 from cn.interpolation import linear_interpolation
 from cn.plot import make_compound_figure
-import cn.functions as fns
+import cn.functions
 
 
 def e7_01():
+    function = cn.functions.fn_1
     range_ = [-1, 1]
-    real_points_number = 10
-    interpolated_points_number = 1000
+    real_points_number = 5
+    interpolated_points_number = 100
 
-    real_points = evaluate(fns.fn_1, linspace(range_, real_points_number))
+    real_points = evaluate(function,
+                           linspace(range_, real_points_number))
 
     poly = linear_interpolation(real_points)
     interpolated_points = tuple((x, poly(x))
