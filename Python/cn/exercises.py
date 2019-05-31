dffabc99b1b372cd793292b8487059fdc98c7e30
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-05-31 13:53:24 +0200

    Add alternative linspace

--- Python/cn/exercises.py
+++ Python/cn/exercises.py
@@ -5,14 +5,34 @@ from cn.plot import make_compound_figure
 import cn.functions
 
 
+def alt_linspace(range_, step_count):
+    min_, max_ = range_
+
+    def x(i):
+        return cos(((2 * (step_count - i)+ 1)
+                    * pi)
+                   / (2 * (step_count + 1)))
+
+    def t(i):
+        return ((max_ - min_)
+                / (2 * x(i)
+                   + ((max_ + min_) / 2)))
+
+    if min_ == -1 and max_ == 1:
+        return tuple(x(i) for i in range(step_count))
+    else:
+        return tuple(t(i) for i in range(step_count))
+
+
 def e7_01():
     function = cn.functions.fn_1
+    space_fn = linspace  # linspace or alt_linspace
     range_ = [-1, 1]
     actual_points_number = 5
     interpolation_points_number = 100
 
-    actual_xs = linspace(range_, actual_points_number)
-    interpolation_xs = linspace(range_, interpolation_points_number)
+    actual_xs = space_fn(range_, actual_points_number)
+    interpolation_xs = space_fn(range_, interpolation_points_number)
 
     actual_points = evaluate(function, actual_xs)
 
