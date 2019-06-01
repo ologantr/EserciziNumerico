675d1bcdc92c3121aaf3085d0240aa3f8d2a00c4
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-06-01 13:00:57 +0200

    Remove not working interpolation implementation and add tests

--- Python/cn/interpolation.py
+++ Python/cn/interpolation.py
@@ -1,55 +1,18 @@
 from cn.arithmetic import product
 
 
-def linear_interpolation_(points):
-    xs, ys = tuple(zip(*points))
-    n = len(points)
-
-    # π(x) = (x - x₀) ··· (x - xₙ)
-    def pi(x):
-        return product((x - x_n) for x_n in xs)
-
-    #           yⱼ
-    # zⱼ = -------------
-    #       n
-    #       ∏   (xⱼ - xᵢ)
-    #      i=0
-    #      i≠j
-    def z(j):
-        def numerator():
-            return ys[j]
-
-        def denominator():
-            return product(xs[j] - xs[i]
-                           for i in range(n)
-                           if i != j)
-
-        return numerator() / denominator()
-
-    #  n      zⱼ
-    #  ∑    ------
-    # j=0   x - xⱼ
-    def sum_(x):
-
-        def numerator(j):
-            return z(j)
-
-        def denominator(j):
-            return x - xs[j]
-
-        return sum(numerator(j) / denominator(j)
-                   for j in range(n))
-
-    #              n     zⱼ
-    # p(x) = π(x)  ∑   ------
-    #             j=0  x - xⱼ
-    def fn(x):
-        return pi(x) * sum_(x)
-
-    return fn
-
-
 def linear_interpolation(points):
+    """
+    Return an interpolation function for the given points.
+
+    >>> fn = linear_interpolation(((0, 0), (1, 1)))
+    >>> fn(2)
+    2.0
+
+    >>> fn = linear_interpolation(((0, 0), (1, 1), (2, 4)))
+    >>> fn(3)
+    9.0
+    """
     xs, ys = tuple(zip(*points))
     n = len(points)
 
