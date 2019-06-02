18417c30d71713b28fd1a5a3f15e66e593292b07
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-06-02 10:41:20 +0200

    Add flake8 support

--- Python/setup.py
+++ Python/setup.py
@@ -1,17 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='calcolo-numerico',
     version='0.0.1',
-
     description='',
-    license='',
-    url='',
 
     author='',
     author_email='',
 
+    license='',
+    url='',
+
     setup_requires=["pytest-runner==4.4", "flake8==3.7.7"],
     install_requires=["matplotlib==3.1.0"],
-    tests_require=["pytest==4.5.0"]
+    tests_require=["pytest==4.5.0"],
+
+    packages=find_packages()
 )
