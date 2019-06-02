2413aeb93bee4ccda9112ec58319b038bcef3806
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-06-02 19:37:54 +0200

    Add Gauss-Seidel method

--- Python/cn/exercises/g6e01.py
+++ Python/cn/exercises/g6e01.py
@@ -49,3 +49,34 @@ def jacobi(matrix, steps):
         result.append(step(result[-1]))
 
     return tuple(result)
+
+
+def seidel(matrix, steps):
+    a, b = matrix
+    order = len(a)
+
+    def x(current, previous, i):
+        def sum_1(current, i):
+            return sum(a[i][j] * current[j]
+                    for j in range(i - 1))
+
+        def sum_2(previous, i):
+            return sum(a[i][j] * previous[j]
+                    for j in range(i + 1, order))
+
+        return (b[i] - sum_1(current, i) - sum_2(previous, i)) / a[i][i]
+
+    def step(previous):
+        current = []
+
+        for i in range(order):
+            current.append(x(current, previous, i))
+
+        return tuple(current)
+
+    result = [step(b)]
+
+    for _ in range(steps - 1 ):
+        result.append(step(result[-1]))
+
+    return tuple(result)
